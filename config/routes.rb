c09b4dfd3cd0190c043df3022160bf3a85c23816
LeslieAine <aineleslie@gmail.com>
2023-06-21 01:55:43 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'root/index'
+  root 'root#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    namespace :v1 do
+      resources :greetings
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
